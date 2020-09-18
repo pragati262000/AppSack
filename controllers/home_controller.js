7628e266bd200b6daa0337dd771f124f2cc30e51
pragati262000 <singhpragati862@gmail.com>
2020-09-18 18:47:23 +0530

    added manual authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 module.exports.home = function (req,res){
+    console.log(req.cookies);
+    //to change value of cookie
+    res.cookie('user_id',25);
     return res.render('home',{
         title: "Home"
     });
--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
+router.get('/sign_up',usersController.signUp);
+router.get('/sign_in',usersController.signIn);
+router.post('/create',usersController.create);
+router.post('/create_session',usersController.createSession);
 module.exports = router;
