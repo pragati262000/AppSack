054ca90371ae49d90bd8bc7c7a0802245cc2ba64
pragati262000 <singhpragati862@gmail.com>
2020-09-18 19:04:11 +0530

    database added

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error Connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log("Connect to Database::MongoDB");
+});
+module.exports = db;
