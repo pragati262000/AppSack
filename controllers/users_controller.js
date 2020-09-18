357d8883be5857d0b946362b742687fc46da28ad
pragati262000 <singhpragati862@gmail.com>
2020-09-18 19:15:21 +0530

    rendered pages for sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,16 @@ module.exports.profile = function (req,res){
         title: "User Profile"
     });
 };
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render("user_sign_up",{
+        title: "Codeial | Sign In "
+    });
+}
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render("user_sign_up",{
+        title: "Codeial | Sign In "
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
+router.get('/sign_up',usersController.signUp);
+router.get('/sign_in',usersController.signIn);
+
 module.exports = router;
