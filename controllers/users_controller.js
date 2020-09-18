1688a944f559ca4b2733452c50de50d5605a132a
pragati262000 <singhpragati862@gmail.com>
2020-09-19 00:00:38 +0530

    added user sign in using passport

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,12 +7,18 @@ module.exports.profile = function (req,res){
 
 //render the sign up page
 module.exports.signUp = function(req,res){
-    return res.render("user_sign_up",{
-        title: "Codeial | Sign In "
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
     });
 }
 //render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render("user_sign_in",{
         title: "Codeial | Sign In "
     });
@@ -40,5 +46,5 @@ module.exports.create = function(req,res){
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req,res){
-    //TODO later
+    return res.redirect('/users/profile');
 }
