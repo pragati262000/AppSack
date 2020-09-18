a912aeb4c0fbb6ccba4d226745d6fe0fa2300c03
pragati262000 <singhpragati862@gmail.com>
2020-09-18 20:46:58 +0530

    added user sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user");
 module.exports.profile = function (req,res){
     return res.render('user_profile',{
         title: "User Profile"
@@ -12,7 +13,32 @@ module.exports.signUp = function(req,res){
 }
 //render the sign in page
 module.exports.signIn = function(req,res){
-    return res.render("user_sign_up",{
+    return res.render("user_sign_in",{
         title: "Codeial | Sign In "
     });
+} 
+
+//get the sign up data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},
+        function(err,user){
+            if(err){console.log('error in finding user in signing up');return}
+            if(!user){
+                console.log("entered");
+                User.create(req.body,function(err,user){
+                   if(err){console.log('error in creating user in signing up');return}
+                   return res.redirect('/users/sign_in');
+                })
+            }else{
+                return res.redirect('back');
+            }
+
+        });
+}
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO later
 }
