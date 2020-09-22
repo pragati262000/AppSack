8805a41e3cffb1e8fa9d31e6a3e71fa3c06b29eb
pragati262000 <singhpragati862@gmail.com>
2020-09-22 17:32:27 +0530

    Saving posts to db and display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,18 @@
-module.exports.home = function (req,res){
-    // console.log(req.cookies);
-    // //to change value of cookie
-    // res.cookie('user-id',25);
-    return res.render('home',{
-        title: "Home"
+const Post = require('../models/post');
+// module.exports.home = function (req,res){
+//     // console.log(req.cookies);
+//     // //to change value of cookie
+//     // res.cookie('user-id',25);
+//     return res.render('home',{
+//         title: "Home"
+//     });
+// };
+module.exports.home = function(req,res){
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+        });
     });
-};
+
+}
